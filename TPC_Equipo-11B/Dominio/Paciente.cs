d28aff6624e39d9261f5ef7f20b4ee196998e955
Anderson-stk <zpozo1912@gmail.com>
2026-05-27 02:05:50 -0300

    En la parte de dominio creacion de las class, Especialidad, Medico, Paciente, Turno, TurnoTrabajo y usuario

---
+++ TPC_Equipo-11B/Dominio/Paciente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio {
+    public class Paciente {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DNI { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string ObraSocial { get; set; }
+        public bool Activo { get; set; } = true;
+
+
+
+    }
+}
---
+++ TPC_Equipo-11B/Dominio/Turno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio {
+    public class Turno {
+
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public int PacienteId { get; set; }
+        public int MedicoId { get; set; }
+        public int Especialidad { get; set; }
+        public DateTime FechaHora { get; set; }
+        public string ObservacionesPaciente { get; set; }
+        public string ObservacionesMedico { get; set; }
+        public EstadoTurno Estado { get; set; } = EstadoTurno.nuevo;
+        public DateTime FechaModificacion { get; set; } = DateTime.Now;
+        public DateTime? FechaModificacion { get; set; }
+
+    }
+}
